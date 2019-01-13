d5364ab7571e5421a04fb468a3cb0b71205e80eb
Tim Göller <tim.goeller@outlook.de>
2019-01-13 21:44:01 +0100

    Define method heads for graph

---
+++ graph.js
@@ -0,0 +1,31 @@
+function Graph() {
+  this.vertices = [];
+
+  /* Vertices */
+
+  this.addVertex = function(id, x, y) {};
+
+  this.removeVertex = function(id) {};
+
+  this.getVertex = function(id) {};
+
+  this.getVertices = function() {};
+
+  /* Edges */
+
+  this.addEdge = function(id, fromID, toID) {};
+
+  this.removeEdge = function() {};
+
+  this.getEdges = function() {};
+
+  this.getEdge = function(id) {};
+
+  this.retrieveEdgeByVertice = function(vertexA, vertexB) {};
+
+  /* Requests */
+
+  this.adjacent = function(vertexA, vertexB) {};
+
+  this.neighbours = function(vertex) {};
+}
