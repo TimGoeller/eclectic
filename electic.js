489c8c01eb71bb93ffe582184e4ee979e7851741
Tim Göller <tim.goeller@outlook.de>
2019-01-14 20:59:04 +0100

    Add graph file drop

--- electic.js
+++ electic.js
@@ -1,3 +1,5 @@
+var graph;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -9,3 +11,30 @@ function windowResized() {
 function draw() {
   background(255);
 }
+
+function createGraphFromTxt(text) {
+  console.log(text);
+  graph = Graph();
+}
+
+window.onload = function() {
+  dropZone = document.getElementById("drop-zone");
+
+  dropZone.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
+
+  dropZone.addEventListener("drop", function(event) {
+    event.preventDefault();
+
+    let file = event.dataTransfer.files[0],
+      reader = new FileReader();
+
+    if (file.type == "text/plain") {
+      reader.readAsText(file);
+      reader.onloadend = function(e) {
+        createGraphFromTxt(reader.result);
+      };
+    }
+  });
+};
