6e05606df40cdd939a1dcc76fb8905d3c8f92172
yannic-bruegger <36576062+yannic-bruegger@users.noreply.github.com>
2019-01-19 17:29:32 +0200

    Change style of highlighted vertex

--- vertex.js
+++ vertex.js
@@ -20,12 +20,14 @@ function Vertex(x = 0, y = 0, value = "") {
     buffer.stroke(83, 216, 251);
     if (this.highlighted) buffer.stroke("#E83562");
     buffer.fill("#DCE1E9");
+    if (this.highlighted) buffer.fill("#E83562");
 
     buffer.strokeWeight(SIZE / 10);
     buffer.ellipse(this.position.x, this.position.y, SIZE);
 
     /* Text with value */
     buffer.fill("#1C3646");
+    if (this.highlighted) buffer.fill("#FFFFFF");
     buffer.strokeWeight(0);
     buffer.textSize(SIZE / 2);
     buffer.textAlign(CENTER, CENTER);
